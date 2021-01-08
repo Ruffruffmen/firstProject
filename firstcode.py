3ab1ac2bca1d79532c12cfc007a11e1fbb28caeb
Ruffruffmen <68183438+Ruffruffmen@users.noreply.github.com>
2021-01-08 04:39:45 -0800

    Update firstcode.py

--- firstcode.py
+++ firstcode.py
@@ -1,1 +1,35 @@
-print("and so it begins")
+iron_club_description = """These irons raise the bar on the gold standard for tour iron excellence. 
+If you demand ultimate playability with consistent distance and forgiveness, 
+these are the irons for you."""
+iron_club_price = 599.99
+
+driver_club_description = """Mid spin and a moderate draw bias should please a wide range of abilities. 
+A single 5-gram screw in the rear portion of the sole bumps up launch and forgiveness."""
+driver_club_price = 499.99
+
+putter_club_description = """Tour-inspired blade and mid-mallet designs that equip golfers with pure performance and a solid milled feel."""
+putter_club_price = 399.99
+
+golf_bag_description = """This versatile bag features detachable, 
+comfortable Equaflex max air dual straps for maximum comfort while carrying you bag."""
+golf_bag_price = 259.99
+
+golf_shoes_description = """Men's golf shoes provide superior feel with Fine Tuned Foam (FTF) 
+for supple cushioning and a infinity outsole design for superior stability."""
+golf_shoes_price = 169.99
+
+golf_glove_description = """4-Way Stretch Synthetic at the knuckles wicks away moisture 
+while increasing breathability and flexibility."""
+golf_glove_price = 19.99
+
+golf_ball_description = """Golf balls designed for maximum distance and the 
+best short game control to help all golfers shoot lower scores."""
+golf_ball_price = 39.99
+
+sales_tax = .088
+
+customer_item_description = () # Insert Item Description in Parenthesis 
+customer_total_price = () # Insert Item Price in Parenthesis
+
+print(customer_item_description)
+print(customer_total_price)
